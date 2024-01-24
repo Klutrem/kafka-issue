070d4d026871704fd7cab35da916acccf23e3197
goodhumored <goodhumored@vk.com>
2024-01-24 13:04:35 +0300

    redundant files removed

--- src/infrastructure/modules/core/cruds/place.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from "@nestjs/common";
-import PlaceController from "../../../../application/place.controller";
-
-@Module({
-    controllers: [PlaceController],
-  })
-export default class PlaceModule {}
