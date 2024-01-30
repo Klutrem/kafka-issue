e3555d97dbfa9d2d810a17857f0368e92a6449a9
klutrem <danyaryabov@mail.ru>
2024-01-30 13:41:23 +0300

    added interceptor that causes async message processing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Global, Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { APP_INTERCEPTOR } from "@nestjs/core";
+import { LoggerInterceptor } from "./logger.interceptor";
 import TestController from "./test.controller";
 
 @Global()
 @Module({
   imports :[ConfigModule.forRoot()],
-  controllers: [TestController]
+  controllers: [TestController],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor
+    }
+  ]
 })
 export default class AppModule {}
